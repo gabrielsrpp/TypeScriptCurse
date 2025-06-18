26d523618348cf41dd979dfc1ff23b599f0be5a0
gabrielsrpp <gabrielpires2003@hotmail.com>
2025-06-18 15:06:21 -0300

    Generics | Constraint em Generics

---
+++ 7_CriacaodeTipos/index.js
@@ -0,0 +1,19 @@
+"use strict";
+// 1 - generics
+function showData(arg) {
+    return `O dado é: ${arg}`;
+}
+console.log(showData(5));
+console.log(showData("testando generic"));
+console.log(showData(true));
+console.log(["teste"]);
+// 2 - Constraint em Generics
+function showProductName(obj) {
+    return `O nome do produto é: ${obj.name}`;
+}
+const myObj = { name: "Porta", cor: "Branca" };
+const otherProduct = { name: "Carro", wheels: 4, engine: 1.0 };
+const thirdObj = { price: 19.99, category: "Roupa" };
+console.log(showProductName(myObj));
+console.log(showProductName(otherProduct));
+// console.log(showProductName(thirdObj)) // precisaria da propiedade name
