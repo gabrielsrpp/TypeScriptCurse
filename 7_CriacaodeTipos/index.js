f449b16abbe33d7a3532af6ee7c1d7603d498db0
gabrielsrpp <gabrielpires2003@hotmail.com>
2025-06-18 15:42:43 -0300

    7_CriacaodeTipos

--- 7_CriacaodeTipos/index.js
+++ 7_CriacaodeTipos/index.js
@@ -20,3 +20,13 @@ const myCar = { name: "Fusca", wheels: 4, engine: 1.0, color: "Branco" };
 const myPen = { name: "Caneta BIC   ", wheels: false, engine: false, color: "Azul" };
 console.log(myCar);
 console.log(myPen);
+// 4 - type parameters
+function getSomeKey(obj, key) {
+    return `A chave ${String(key)} está presente no objeto e tem o valor de ${obj[key]} `;
+}
+const server = {
+    hd: '2TB',
+    ram: '32GB'
+};
+console.log(getSomeKey(server, 'ram'));
+// console.log(getSomeKey(server, 'teste')) // tenque estar presente no objeto
