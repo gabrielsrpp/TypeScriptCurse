c1e3af13045dc7f60b012317cbeda9528827c2eb
gabrielsrpp <gabrielpires2003@hotmail.com>
2025-06-18 15:21:41 -0300

    Generics com interface

--- 7_CriacaodeTipos/index.js
+++ 7_CriacaodeTipos/index.js
@@ -16,4 +16,7 @@ const otherProduct = { name: "Carro", wheels: 4, engine: 1.0 };
 const thirdObj = { price: 19.99, category: "Roupa" };
 console.log(showProductName(myObj));
 console.log(showProductName(otherProduct));
-// console.log(showProductName(thirdObj)) // precisaria da propiedade name
+const myCar = { name: "Fusca", wheels: 4, engine: 1.0, color: "Branco" };
+const myPen = { name: "Caneta BIC   ", wheels: false, engine: false, color: "Azul" };
+console.log(myCar);
+console.log(myPen);
--- 7_CriacaodeTipos/index.ts
+++ 7_CriacaodeTipos/index.ts
@@ -22,3 +22,20 @@ const thirdObj = {price: 19.99, category: "Roupa"}
 console.log(showProductName(myObj))
 console.log(showProductName(otherProduct))
 // console.log(showProductName(thirdObj)) // precisaria da propiedade name
+
+// 3 - generics com interface
+interface MyObject<T,U,Q> {
+    name: string
+    wheels: T
+    engine: U
+    color: Q
+}
+
+type Car = MyObject<number,number,string>
+type Pen = MyObject<boolean, boolean,string>
+
+const myCar:Car = {name: "Fusca", wheels: 4, engine: 1.0, color: "Branco"}
+const myPen: Pen = {name: "Caneta BIC   ", wheels:false, engine:false,color:"Azul"}
+
+console.log(myCar)
+console.log(myPen)
