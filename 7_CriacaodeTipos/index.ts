9514bfa7d15729c6353ce151eada7bfc7036224f
gabrielsrpp <gabrielpires2003@hotmail.com>
2025-06-20 09:41:48 -0300

    Template literals type

--- 7_CriacaodeTipos/index.ts
+++ 7_CriacaodeTipos/index.ts
@@ -123,3 +123,17 @@ const someVar:myType = 5
 // const someVar:myType = "teste"
 
 type myTypeB = Teste extends {showNumber(): number } ? string : boolean
+
+// 9 - template literals type
+
+type testA = "text"
+
+type CustomType = `some ${testA}`
+
+const testing: CustomType = "some text"
+// const testing2: CustomType = "some text 2"
+
+type a1 = "Testando"
+type a2 = "Union"
+
+type a3 = `${a1}` | `${a2}`
