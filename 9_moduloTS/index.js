286af8e69cb4527560f8dbd57ad37d23c57a97ab
gabrielsrpp <gabrielpires2003@hotmail.com>
2025-06-20 15:51:20 -0300

    Importação de vaiaveis

--- 9_moduloTS/index.js
+++ 9_moduloTS/index.js
@@ -3,5 +3,9 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+// 1 - importação de arquivos
 const greet_js_1 = __importDefault(require("./greet.js"));
 (0, greet_js_1.default)();
+// 2 - import de variavel
+const variable_1 = require("./variable");
+console.log(variable_1.x);
--- 9_moduloTS/index.ts
+++ 9_moduloTS/index.ts
@@ -1,3 +1,9 @@
+// 1 - importação de arquivos
 import importGreet from "./greet.js";
 
 importGreet();
+
+// 2 - import de variavel
+import {x} from "./variable";
+
+console.log(x);
