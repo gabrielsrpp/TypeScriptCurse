6e647e94b8ac8079c29e1e8a6bb6520b8b1e9761
gabrielsrpp <gabrielpires2003@hotmail.com>
2025-06-20 10:37:21 -0300

    Herança e Super

--- 8_ClassesTypeScript/index.js
+++ 8_ClassesTypeScript/index.js
@@ -1,3 +1,18 @@
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        if (typeof b !== "function" && b !== null)
+            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
 // 1 - campos em classe
 var User = /** @class */ (function () {
     function User() {
@@ -33,3 +48,23 @@ console.log(fusca);
 console.log(fusca.wheels);
 fusca.name = "Fusca Turbo";
 // fusca wheels = 5
+// 4 - Herança e super
+var Machine = /** @class */ (function () {
+    function Machine(name) {
+        this.name = name;
+    }
+    return Machine;
+}());
+var trator = new Machine("Trator");
+var KillerMachine = /** @class */ (function (_super) {
+    __extends(KillerMachine, _super);
+    function KillerMachine(name, guns) {
+        var _this = _super.call(this, name) || this;
+        _this.guns = guns;
+        return _this;
+    }
+    return KillerMachine;
+}(Machine));
+var destroyer = new KillerMachine("Destroyer", 4);
+console.log(trator);
+console.log(destroyer);
--- 8_ClassesTypeScript/index.ts
+++ 8_ClassesTypeScript/index.ts
@@ -51,3 +51,30 @@ console.log(fusca.wheels)
 fusca.name = "Fusca Turbo"
 
 // fusca wheels = 5
+
+
+// 4 - Herança e super
+
+class Machine {
+    name
+
+    constructor(name:string) {
+        this.name = name
+    }
+}
+
+const trator = new Machine("Trator")
+
+class KillerMachine extends Machine {
+    guns
+
+    constructor(name:string, guns: number) {
+        super(name)
+        this.guns = guns
+    }
+}
+
+const destroyer = new KillerMachine("Destroyer", 4)
+
+console.log(trator)
+console.log(destroyer)
