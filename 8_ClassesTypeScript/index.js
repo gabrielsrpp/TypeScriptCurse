95814c5e5ee4aa12532f9f66be94bd6e04c93a5d
gabrielsrpp <gabrielpires2003@hotmail.com>
2025-06-20 10:22:17 -0300

    Propiedades readonly

--- 8_ClassesTypeScript/index.js
+++ 8_ClassesTypeScript/index.js
@@ -1,19 +1,35 @@
-"use strict";
 // 1 - campos em classe
-class User {
-}
-const teste = new User();
+var User = /** @class */ (function () {
+    function User() {
+    }
+    return User;
+}());
+var teste = new User();
 console.log(teste);
 teste.name = "teste";
 // teste.job = "Lixeiro"
 console.log(teste);
 // 2 - constructor
-class NewUser {
-    constructor(name, age) {
+var NewUser = /** @class */ (function () {
+    function NewUser(name, age) {
         this.name = name;
         this.age = age;
     }
-}
-const Paulo = new NewUser("teste", 22);
+    return NewUser;
+}());
+var Paulo = new NewUser("teste", 22);
 console.log(Paulo);
 // const pedro = new NewUser(12,12)
+// 3 - compo readonly
+var Car = /** @class */ (function () {
+    function Car(name) {
+        this.wheels = 4;
+        this.name = name;
+    }
+    return Car;
+}());
+var fusca = new Car("Fusca");
+console.log(fusca);
+console.log(fusca.wheels);
+fusca.name = "Fusca Turbo";
+// fusca wheels = 5
--- 8_ClassesTypeScript/index.ts
+++ 8_ClassesTypeScript/index.ts
@@ -30,3 +30,24 @@ const Paulo = new NewUser("teste", 22)
 console.log(Paulo)
 
 // const pedro = new NewUser(12,12)
+
+// 3 - compo readonly
+
+class Car {
+    name
+    readonly wheels = 4
+
+    constructor(name: string) {
+        this.name = name
+    }
+}
+
+const fusca = new Car("Fusca")
+
+console.log(fusca)
+
+console.log(fusca.wheels)
+
+fusca.name = "Fusca Turbo"
+
+// fusca wheels = 5
