34761ffcc34f65df0e7ad7528e130b4600917c76
gabrielsrpp <gabrielpires2003@hotmail.com>
2025-06-23 14:48:26 -0300

    Funções em componentes

--- 11_react_com_ts/src/App.tsx
+++ 11_react_com_ts/src/App.tsx
@@ -9,13 +9,19 @@ const name: string = "Paulo";
 const age: number = 30;
 const isWorking: boolean = true;
 
+// 2- funções
+const userGreeting = (name: string): string => {
+  return `Olá, ${name}!`;
+};
+
 
   return (
     <div className="App">
       <h1>TypeScript com React</h1>
       <h2>Nome: {name}</h2>
       <p>Idade: {age}</p>
-      {isWorking && (<p>esta trabalhando!</p>)}
+      {isWorking && <p>Está trabalhando!</p>}
+      <h3>{userGreeting(name)}</h3>
     </div>
   );
 }
