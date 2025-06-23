01ad7c52cf5c265a8c8b96e8199254e6d21fe606
gabrielsrpp <gabrielpires2003@hotmail.com>
2025-06-23 10:07:09 -0300

    Criando o primeiro decorator

---
+++ 10_Decorators/index.ts
@@ -0,0 +1,27 @@
+// 1 - exemplo decorator
+function myDecorator() {
+
+    console.log("Iniciando decorator!");
+
+    return function(
+        target: any,
+        propertKey: string,
+        descriptor: PropertyDescriptor
+      ) {
+        console.log("Executando decorator")
+        console.log(target);
+        console.log(propertKey);
+        console.log(descriptor);
+    };
+}
+
+class myClass {
+    @myDecorator()
+    testing() {
+        console.log("terminando execução do metodo");
+    }
+}
+
+const myObj = new myClass();
+
+myObj.testing();
