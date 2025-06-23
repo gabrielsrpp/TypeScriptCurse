9dd40e4b8d44db7ea8b82c47c1aab41dc5f4eb84
gabrielsrpp <gabrielpires2003@hotmail.com>
2025-06-23 14:37:24 -0300

    Variaveis no React

--- 11_react_com_ts/src/App.tsx
+++ 11_react_com_ts/src/App.tsx
@@ -3,9 +3,19 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+// 1 - variaveis
+const name: string = "Paulo";
+const age: number = 30;
+const isWorking: boolean = true;
+
+
   return (
     <div className="App">
       <h1>TypeScript com React</h1>
+      <h2>Nome: {name}</h2>
+      <p>Idade: {age}</p>
+      {isWorking && (<p>esta trabalhando!</p>)}
     </div>
   );
 }
