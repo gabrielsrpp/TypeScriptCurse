1ffa1ef58632683cd4738073daadbee0daee9e38
gabrielsrpp <gabrielpires2003@hotmail.com>
2025-07-01 10:44:25 -0300

    Iniciando o express

--- 12_express_com_ts/src/app.ts
+++ 12_express_com_ts/src/app.ts
@@ -1,3 +1,15 @@
 // 1 - iniciando projeto
-console.log("Express + TS!!!");
+// console.log("Express + TS!!!");
 
+// 2 - init express
+import express from "express"
+
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send("Hello Express!");
+});
+
+app.listen(3000, () => {
+    console.log ("Aplicação de TS + Express funcionando !");
+});
