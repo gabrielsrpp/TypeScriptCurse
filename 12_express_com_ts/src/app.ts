1f97d531d88b320780e9a539f64f1762392fc45d
gabrielsrpp <gabrielpires2003@hotmail.com>
2025-07-02 10:27:03 -0300

    Tratando erros

--- 12_express_com_ts/src/app.ts
+++ 12_express_com_ts/src/app.ts
@@ -131,6 +131,21 @@ app.get("/api/user/:id/details/:name", (
 });
 
 
+// 13 - tratando erros
+app.get("/api/error", (req: Request, res: Response) => {
+
+
+    try {
+        // a nossa lógica
+        throw new Error("Algo deu errado!")
+    } catch (e: any) {
+
+        res.status(500).json({msg: e.message})
+    }
+
+})
+
+
 
 app.listen(3000, () => {
     console.log ("Aplicação de TS + Express funcionando !");
