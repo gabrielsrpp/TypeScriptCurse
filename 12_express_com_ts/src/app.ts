ef805fa83922c5a6f041dcad2cca710b328874f5
gabrielsrpp <gabrielpires2003@hotmail.com>
2025-07-02 10:18:13 -0300

    Interfaces de Requisição e Resposta

--- 12_express_com_ts/src/app.ts
+++ 12_express_com_ts/src/app.ts
@@ -118,6 +118,19 @@ app.get("/api/user/:id/access", checkUser, (req: Request, res: Response) => {
     res.json({ msg: "Bem-vindo a área administrativa!"});
 });
 
+// 12 - req e res com generics
+app.get("/api/user/:id/details/:name", (
+    req: Request<{ id: string; name: string }>, 
+    res: Response<{status: boolean}>
+) => {
+    console.log(`ID: ${req.params.id}`);
+    console.log(`Name: ${req.params.name}`);
+
+
+    res.json({ status: true });
+});
+
+
 
 app.listen(3000, () => {
     console.log ("Aplicação de TS + Express funcionando !");
