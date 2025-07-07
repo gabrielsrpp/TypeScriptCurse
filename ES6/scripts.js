f563319f4d436f5d09d2cf88876ce18ed9d6a195
gabrielsrpp <gabrielpires2003@hotmail.com>
2025-07-07 09:47:25 -0300

    Spread operator

--- ES6/scripts.js
+++ ES6/scripts.js
@@ -145,3 +145,23 @@ const {name: productName, price, category: productCategory, color} = productDeta
 
 console.log(`O nome do produto é ${productName}, custa R$${price}, pertence a categoria ${productCategory} e é da cor ${color}`,)
 
+// 7 Spread operator
+
+const a1 = [1,2,3]
+const a2 = [4,5,6]
+
+const a3 = [...a1, ...a2]
+
+console.log(a3)
+
+const a4 = [0,...a1,4]
+
+console.log(a4)
+
+const carName = {name: 'Gol'}
+const carBrand = {brand: 'VW'}
+const otherInfos =  {km: 1000000000, price: 490000000000}
+
+const car = { ...carName, ...carBrand, ...otherInfos, wheels: 4}
+
+console.log(car)
